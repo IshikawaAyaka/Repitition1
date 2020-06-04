04f6316d3b52425fdb530fd713ad6f59ea93c6c2
Shino Harada <sh-harada@cresco.co.jp>
2020-06-04 15:27:05 +0900

    massage

--- 04_実装／単体テスト/Turn.java
+++ 04_実装／単体テスト/Turn.java
@@ -1,23 +1,38 @@
-public class Turn {
+package Sanmoku;
 
+public class Turn {
+	
 	private Player player;
 
 	private Board board;
+	
+	private int x;
+	private int y;
+	private Mark mark;
+	
+	public Turn(Player player) {
+		this.player = player;
+	}
 
 	public void setMark() {
-
+		board.setMark(x,y,player.getMark());
 	}
 
-	public void showTurn() {
-
+	public int[] requestXY(Board board) {
+		this.board = board;
+		int[] a ;
+		while(true){
+			a = player.setXY();
+			this.x = a[0];
+			this.y = a[1];
+			if(board.canInput(x,y) == true) {
+				break;
+			}
+		}
+		
+		
+		return a;
 	}
 
-	public int requestXY(Board board) {
-		return 0;
-	}
-
-	public Turn(Player player) {
-
-	}
 
 }
