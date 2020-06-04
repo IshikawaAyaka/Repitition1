e82c9fd0f58041feef4c26d0abc5c457e024f44d
YoshitomiAyano <a-yoshitomi@cresco.co.jp>
2020-06-04 12:28:44 +0900

    変更追加

--- 04_実装／単体テスト/Line.java
+++ 04_実装／単体テスト/Line.java
@@ -1,13 +1,29 @@
+package tictactoe;
+
 public class Line {
+	private Square[] squares = new Square[3];
 
-	private Square[] squares;
-
-	public Line(Square square1, Square square2, Square square3) {
+	public Line() {
 
 	}
 
 	public boolean judgeWinner() {
+		if (squares[0].getMark() == Mark.CIRCLE && squares[1].getMark() == Mark.CIRCLE
+				&& squares[2].getMark() == Mark.CIRCLE) {
+			return true;
+		}
+
+		if (squares[0].getMark() == Mark.CROSS && squares[1].getMark() == Mark.CROSS
+				&& squares[2].getMark() == Mark.CROSS) {
+			return true;
+		}
 		return false;
+	}
+
+	public void setSquare(Square square1, Square square2, Square square3) {
+		squares[0] = square1;
+		squares[1] = square2;
+		squares[2] = square3;
 	}
 
 }
