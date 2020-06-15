d3e7d61c6d5e23a657bf77e5889d8b3b793299f3
Ishikawa Ayaka <a-ishikawa@cresco.co.jp>
2020-06-15 12:28:39 +0900

    フォルダ作成

---
+++ 04_実装／単体テスト/テストコード/TicTacToeTest.java
@@ -0,0 +1,19 @@
+package tictactoe;
+
+import org.junit.Test;
+
+public class TicTacToeTest {
+
+
+	@Test
+	public void test1() {
+		TicTacToe ttt = new TicTacToe();
+		ttt.game();
+	}
+
+	@Test
+	public void test2() {
+		TicTacToe ttt = new TicTacToe();
+		ttt.game();
+	}
+}
